177aeb26e67001f33db4e1a06d30f62e6406b87d
igor.kudlo <igor.kudlo@spiralscout.com>
2022-10-18 12:08:27 +0300

    add user card status

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  login: "IgorKudlo",
+  id: 12099835,
+  avatar: "https://avatars.githubusercontent.com/u/12099835?v=4",
+  name: "IgorKudlo",
+  company: null,
+  blog: "",
+  location: null,
+  bio: null,
+  twitter: null,
+  repos: 46,
+  followers: 0,
+  following: 0,
+  created: "2015-04-24T13:26:00Z",
+}
