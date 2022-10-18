fdae1c58c43136188fd0358700cf14a5dca4cfbe
igor.kudlo <igor.kudlo@spiralscout.com>
2022-10-18 15:10:49 +0300

    add user title

---
+++ src/components/UserTitle/UserTitle.tsx
@@ -0,0 +1,25 @@
+import styles from './UserTitle.module.scss';
+import {LocalGithubUser} from '../../types';
+
+interface UserTitleProps extends Pick<
+  LocalGithubUser,
+  'login' | 'name' | 'created'
+>{}
+
+const localDate = new Intl.DateTimeFormat('en-Gb', {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric',
+});
+
+export const UserTitle = ({ login, name, created }: UserTitleProps) => {
+  const joinedData = localDate.format(new Date(created));
+
+  return (
+    <div className={styles.userTitle}>
+      <h2>{name}</h2>
+      <h3>{login}</h3>
+      <span>{joinedData}</span>
+    </div>
+  );
+}
